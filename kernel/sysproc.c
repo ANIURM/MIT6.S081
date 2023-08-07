969dfa391096f93522a75707dd8d424ac3ed1830
ANIURM <534147085@qq.com>
2023-08-07 10:37:59 +0800

    feat: implement trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -99,6 +99,10 @@ sys_uptime(void)
 uint64
 sys_trace(void)
 {
-    int pid = myproc()->pid;
-
+    int mask;
+    if (argint(0, &mask) < 0) {
+        return -1;
+    }
+    myproc()->mask = mask;
+    return 0;
 }
