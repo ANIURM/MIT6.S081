f9d04463545103c400850ee3e4fdfb92055d819e
Aniurm <534147085@qq.com>
2023-10-26 10:44:42 +0800

    refactor: freewalk

--- kernel/vm.c
+++ kernel/vm.c
@@ -292,13 +292,16 @@ freewalk(pagetable_t pagetable, int ignore_leaf)
   // there are 2^9 = 512 PTEs in a page table.
   for(int i = 0; i < 512; i++){
     pte_t pte = pagetable[i];
-    if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+    if (!(pte & PTE_V)) {
+      continue;
+    }
+    if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
       // this PTE points to a lower-level page table.
       uint64 child = PTE2PA(pte);
       freewalk((pagetable_t)child, ignore_leaf);
       pagetable[i] = 0;
-    } else if (!ignore_leaf) {
-      if (pte & PTE_V) {
+    } else {
+      if (!ignore_leaf) {
         panic("freewalk: leaf");
       }
     }
