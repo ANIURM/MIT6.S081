57a72f44c6626fcb2df644882da051e3943eda44
Aniurm <534147085@qq.com>
2023-11-04 20:36:45 +0800

    fix: handle not-allocated user addr

--- kernel/vm.c
+++ kernel/vm.c
@@ -357,9 +357,13 @@ int
 copyout(pagetable_t pagetable, uint64 dstva, char *src, uint64 len)
 {
   uint64 n, va0, pa0;
+  struct proc *p = myproc();
 
   while(len > 0){
     va0 = PGROUNDDOWN(dstva);
+    if (uvmshouldtouch(p, va0)) {
+      uvmlazytouch(p, va0);
+    }
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
@@ -382,9 +386,13 @@ int
 copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
 {
   uint64 n, va0, pa0;
+  struct proc *p = myproc();
 
   while(len > 0){
     va0 = PGROUNDDOWN(srcva);
+      if (uvmshouldtouch(p, va0)) {
+      uvmlazytouch(p, va0);
+    }
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
       return -1;
